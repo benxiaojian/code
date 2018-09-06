49568c41a7c820a5b1f433d738c68776fdfff44c
dengzijian <dengzijian@casachina.com.cn>
2018-09-06 11:02:48 +0800

    Sliding Window

---
+++ leetcode/num_3.cpp
@@ -0,0 +1,59 @@
+/*
+ * two_sum.c
+ *
+ *      Author: Nine
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdlib.h>
+#include <cstring>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int max = 0;
+        int characters[256] = {0};
+        int sum = 0;
+        int i,j;
+
+        for (i = 0, j = 0; j < s.length();) {
+            int c = s[j];
+
+            if (characters[c] == 0) {
+                sum++;
+                characters[c]++;
+                j++;
+            } else {
+                if (max < sum) {
+                    max = sum;
+                }
+
+                // remove left chart, until not repeat
+                if (characters[c] > 0) {
+                    characters[s[i]]--;
+                    i++;
+                    sum--;
+                }
+            }
+        }
+
+        if (max < sum) {
+            return sum;
+        }
+
+        return max;
+    }
+
+};
+
+
+int main()
+{
+    // dvfdf
+
+    Solution s;
+
+    cout << s.lengthOfLongestSubstring("dvfdf") << endl;
+}
