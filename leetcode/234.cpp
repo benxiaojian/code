f435cc63823118066a604f69bf57b6f6ba2143dc
dengzijian <dengzijian@casachina.com.cn>
2018-09-21 13:22:53 +0800

    判断list是否回文，用函数的栈来回退指针

---
+++ leetcode/234.cpp
@@ -0,0 +1,52 @@
+/*
+ * 回文列表
+ *
+ *      Author: Nine
+ */
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+// 要求O(n)的时间和O(1)的空间
+
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+private:
+    bool isPal(ListNode *head)
+    {
+        if (head == NULL) {
+            return true;
+        }
+
+        if (!isPal(head->next)) {
+            return false;
+        }
+
+        if (head->val != this->ptr->val) {
+            return false;
+        }
+
+        this->ptr = this->ptr->next;
+        return true;
+    }
+
+public:
+    bool isPalindrome(ListNode* head) {
+        this->ptr = head;
+
+        return isPal(head);
+    }
+private:
+    ListNode *ptr;
+};
